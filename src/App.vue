779e2658f5e8bf26e3deb7466488923f81fddee9
rizal <rizalpratama10010@gmail.com>
2019-12-26 19:15:04 +0700

    calculator created

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,3 @@
-<!-- <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template> -->
-
 <template>
   <div id="app">
     <Calculator/>
@@ -15,18 +5,5 @@
 </template>
 
 <script>
-  import Calculator from "./components/calculator"
-
-  export default {
-    name: "app",
-    components: {
-      Calculator
-    }
-  }
+  
 </script>
-
-<style>
-  .has-full-height{
-    height: 100vh;
-  }
-</style>
